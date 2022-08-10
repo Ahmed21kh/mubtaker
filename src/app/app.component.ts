46eab1a0ba0c65aaf138aee772273739f3197031
Ahmed <92892926+Ahmed21kh@users.noreply.github.com>
2022-08-10 22:00:26 +0200

    project is added to githup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
+import { LoginModel } from 'src/app/models/login';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'projectdream-app';
+  public confirmpass:any;
+  public qrcodetext:string
+   public login :LoginModel;
+  collapsed:boolean=true;
+  dropdown:boolean=true
+  title = 'myapplicatin-app';
+  constructor( private rote : Router){
+    this.login=new LoginModel();
+  }
+  goToRegist(){
+    this.rote.navigate(["/register"])
+  }
+  goToUser(){
+    this.rote.navigate(["/user"])
+  }
+  onSubmit(_form:NgForm){
+
+    console.log(this.login);
+
+    // this.User.push(this.login.email, this.login.password)
+
+  }
 }
